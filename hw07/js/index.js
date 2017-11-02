474c19263f4c248ca068053d8661556cea72b915
Ej Cruz <ejatbcc@gmail.com>
2017-11-02 16:14:57 -0700

    Completed homework 7 on 11.2.2017. Late and delayed. Code finally succeeded

--- hw07/js/index.js
+++ hw07/js/index.js
@@ -6,8 +6,19 @@
 // parameter. In the body of the function, there should be code that
 // appends an "li" element, with the string inside, to the un-ordered
 // list ("ul" tag):
+const addListElementToOutput = (string)=>{
+  const getUnordered = document.getElementById('output')
+  const liElement= document.createElement('li')
+  const liText = document.createTextNode(string)
+  liElement.appendChild(liText)
+  getUnordered.appendChild(liElement)
+}
 
-
+// addListElementToOutput('this took a danglongtime')
+// addListElementToOutput('i tie red. uhm tired')
+// addListElementToOutput('keep it going')
+// addListElementToOutput('giving up is real failure')
+// addListElementToOutput('....and better late than never!!')
 
 //------------------------------------------------------------------
 // PART II
@@ -19,3 +30,15 @@
 // 2. If the number is divisible by 5, append "Buzz" to the unordered list
 // 3. If the number is divisible by 3 AND 5, append "FizzBuzz" to the unordered list.
 // 4. Bonus points: Include both the number *and* the word you've appended to the list
+
+for (var i=1; i<=100; i++){
+  if ((i%3===0) && (i%5===0)) {
+    addListElementToOutput('FizzBuzz')
+  }
+  else if(i%3===0){
+    addListElementToOutput('Fizz')
+  }
+  else if (i%5===0) {
+    addListElementToOutput('Buzz')
+  }
+}
